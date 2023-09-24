d15e8a8094b0d966309b06d43c525578ed19b66b
Arya <arya.tesla1856@gmail.com>
2023-09-24 08:13:32 -0700

    rewrote all html and css and now It's readable for others

--- Branch/views.py
+++ Branch/views.py
@@ -28,12 +28,10 @@ def home(request):
 	if request.method == "POST" and "button-input" in request.POST:
 		try:
 			user_name = request.POST.get("username-input")
-			foll = requests.get(f"https://api.github.com/users/{user_name}/followers").json()
-			half = int(len(foll)/2)
-			print(half)
-			followers1 = [follower["login"] for follower in foll[0:half]]
-			followers2 = [follower["login"] for follower in foll[half:]]
 			response = requests.get(f"https://api.github.com/users/{user_name}").json()
+			foll = requests.get(f"https://api.github.com/users/{user_name}/followers").json()
+			followers1 = [follower["login"] for follower in foll[0:int(len(foll)/2)]]
+			followers2 = [follower["login"] for follower in foll[int(len(foll)/2):]]
 			context = {
 				"user_username" : response["login"],
 				"user_name" : response["name"],
@@ -49,11 +47,11 @@ def home(request):
 				"user_blog" : response["blog"],
 				"user_twitter" : response["twitter_username"],
 				"user_email" : response["email"],
-				"theme_right_now" : "",
+				"current_theme" : "",
 				"followers_part1" : followers1,
 				"followers_part2" : followers2,
 			}
-			context.update({"theme_right_now" : last_theme })
+			context.update({"current_theme" : last_theme })
 			return HttpResponseRedirect("dashboard")
 		except:
 			return HttpResponseRedirect("/")
@@ -63,14 +61,15 @@ def dashboard(request):
 	global context
 	global last_theme
 
-	if (request.method == "POST" and "theme-option-button-dark" in request.POST):
-		if (context["theme_right_now"] == "dark"):
-			context.update({"theme_right_now" : "light"})
+	if (request.method == "POST" and "change-theme-button" in request.POST):
+		print("*****************************", context["current_theme"])
+		if (context["current_theme"] == "dark"):
+			context.update({"current_theme" : "light"})
 			Theme.objects.all().delete()
 			Theme.objects.create(theme="Light")
 			last_theme = "light"
-		elif (context["theme_right_now"] == "light"):
-			context.update({"theme_right_now" : "dark"})
+		elif (context["current_theme"] == "light"):
+			context.update({"current_theme" : "dark"})
 			Theme.objects.all().delete()
 			Theme.objects.create(theme="Dark")
 			last_theme = "dark"
