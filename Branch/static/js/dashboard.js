d199c3c5a194b1f885441fcc3cf70221e1a5374d
Arya <arya.tesla1856@gmail.com>
2023-09-24 09:54:24 -0700

    created two main structures for personal info and details

--- Branch/static/js/dashboard.js
+++ Branch/static/js/dashboard.js
@@ -1,9 +1,1 @@
 AOS.init()
-function show_followers(){
-  document.getElementById("user-text-data").style.display = "none"
-  document.getElementById("user-followers-data-scroll").style.display = "flex"
-}
-function show_info(){
-  document.getElementById("user-text-data").style.display = "flex"
-  document.getElementById("user-followers-data-scroll").style.display = "none"
-}
